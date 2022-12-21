efc5b60d4f2883de8621d58c28d83c0d4c291916
aliyurekli32 <adamjesperblack@gmail.com>
2022-12-21 20:36:58 +0300

    new files

--- fscohort/models.py
+++ fscohort/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    number = models.PositiveSmallIntegerField()
